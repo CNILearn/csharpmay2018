24e11018df35964fba974564c86d75e4a41cc5ef
Christian Nagel <christian@christiannagel.com>
2018-05-28 13:12:10 +0200

    patterns samples

---
+++ Patterns/PatternsSample/PatternsSample/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PatternsSample
+{
+    public class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public Person(string firstName, string lastName)
+            => (FirstName, LastName) = (firstName, lastName);
+
+        public void Deconstruct(out string firstName, out string lastName)
+            => (firstName, lastName) = (FirstName, LastName);
+
+    }
+
+    public class Professor : Person
+    {
+        public string Subject { get; }
+        public Professor(string firstName, string lastName, string subject)
+            : base(firstName, lastName)
+            => Subject = subject;
+
+        public void Deconstruct(out string firstName, out string lastName, out string subject)
+            => (firstName, lastName, subject) = (FirstName, LastName, Subject);
+    }
+
+    public class Student : Person
+    {
+        public Professor Advisor { get; }
+        public Student(string firstName, string lastName, Professor advisor)
+            : base(firstName, lastName)
+            => Advisor = advisor;
+
+        public void Deconstruct(out string firstName, out string lastName, out Professor advisor)
+            => (firstName, lastName, advisor) = (FirstName, LastName, Advisor);
+
+    }
+}
---
+++ Patterns/PatternsSample/PatternsSample/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace PatternsSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var professor = new Professor("Mads", "Torgersen", "C#");
+            var people = new[]
+            {
+                professor,
+                new Student("Phillip", "Carter", professor),
+                new Person("Dustin", "Campbell")
+            };
+
+            foreach (var p in people)
+            {
+                Console.WriteLine(M3(p));
+            }
+        }
+
+        static string M(Person person)
+        {
+            switch(person)
+            {
+                case Professor p:
+                    return $"Dr. {p.LastName}, Professor of {p.Subject}";
+                case Student s:
+                    return $"{s.FirstName}, student of Dr. {s.Advisor.LastName}";
+                case Person p when p.LastName == "Campbell":
+                    return $"Please enroll {p.FirstName}";
+                case _:
+                    return "come back next year";
+            }
+        }
+
+        static string M2(Person person) =>
+            person switch
+            {
+                Professor p => $"Dr. {p.LastName}, Professor of {p.Subject}",
+                Student s => $"{s.FirstName}, student of Dr. {s.Advisor.LastName}",
+                Person p when p.LastName == "Campbell" => $"Please enroll {p.FirstName}",
+                _ => "come back next year"
+            };
+
+        static string M3(Person person) =>
+            person switch
+            {
+                Professor (_, var ln, var s) => $"Dr. {ln}, Professor of {s}",  // deconstructor of Professor, discard pattern
+                Student (var fn, _, var (_, ln, _)) => $"{fn}, student of Dr. {ln}",  // recursive pattern
+                { LastName: "Campbell", FirstName: var fn } => $"Please enroll {fn}",  // property pattern
+                _ => "come back next year"
+            };
+
+    }
+}
