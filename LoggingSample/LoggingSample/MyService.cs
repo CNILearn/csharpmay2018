42659f3596b2f1f9991fbe39d4563db7deddb67e
Christian Nagel <christian@christiannagel.com>
2018-05-29 12:59:09 +0200

    logging

---
+++ LoggingSample/LoggingSample/MyService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggingSample
+{
+    public class MyService
+    {
+        private readonly ILogger _logger;
+        public MyService(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<MyService>();
+        }
+
+        public void MyMethod()
+        {
+            _logger.LogError("some error in MyMethod");
+        }
+    }
+}
---
+++ LoggingSample/LoggingSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace LoggingSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigureServices();
+            var service = Container.GetService<MyService>();
+            service.MyMethod();
+        }
+
+        private static void ConfigureServices()
+        {
+            var services = new ServiceCollection();
+            services.AddLogging(builder =>
+            {
+                builder.AddDebug();
+                builder.AddConsole();
+            });
+            services.AddTransient<MyService>();
+            Container = services.BuildServiceProvider();
+        }
+        public static IServiceProvider Container { get; private set; }
+    }
+}
